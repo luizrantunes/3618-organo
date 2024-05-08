29bdead264e9db9b45d7b995d4ef90691429cf7c
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-08 16:54:47 -0300

    Vídeo 2.4 - Testando lançamento de erro

--- src/app/services/livro.service.spec.ts
+++ src/app/services/livro.service.spec.ts
@@ -1,6 +1,6 @@
-import { Livro } from "../componentes/livro/livro";
+import { GeneroLiterario, Livro } from "../componentes/livro/livro";
 import { livros } from "../mock-livros";
-import { LivroService } from "./livro.service"
+import { ErroGeneroLiterario, LivroService } from "./livro.service"
 
 describe('LivroService', () => {
   let service: LivroService;
@@ -32,5 +32,30 @@ describe('LivroService', () => {
     const livrosPorGenero = service.obterLivrosPorGenero('romance');
     const livrosEsperados = livros.filter(livro => livro.genero.id === 'romance')
     expect(livrosPorGenero).toEqual(livrosEsperados);
+  });
+
+  it('deveria inicializar os gêneros corretamente', () => {
+    const generosEsperados: GeneroLiterario[] = [
+      { id: 'romance', value: 'Romance' },
+      { id: 'misterio', value: 'Mistério' },
+      { id: 'fantasia', value: 'Fantasia' },
+      { id: 'ficcao-cientifica', value: 'Ficção Científica' },
+      { id: 'tecnicos', value: 'Técnicos' },
+    ];
+
+    expect(service.generos).toEqual(generosEsperados);
+  });
+
+  it('deveria lançar um erro ao tentar cadastrar um livro com gênero desconhecido', () => {
+    const novoLivro: Livro = {
+      titulo: 'Novo Livro',
+      autoria: 'Autor Desconhecido',
+      imagem: 'http://example.com/cover.jpg',
+      genero: {id: 'nao-existe', value: 'Não Existe'},
+      dataLeitura: '2024-04-19',
+      classificacao: 5
+    };
+
+    expect(() => service.adicionarLivro(novoLivro)).toThrow(ErroGeneroLiterario);
   })
 })
