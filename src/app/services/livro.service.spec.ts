95ec7f64512080515fb0e5608b3e32b5b255c0bd
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-08 15:57:14 -0300

    Vídeo 2.1 - padrão AAA

---
+++ src/app/services/livro.service.spec.ts
@@ -0,0 +1,12 @@
+import { LivroService } from "./livro.service"
+
+describe('LivroService', () => {
+  let service: LivroService;
+
+  it('deveria ser criado', () => {
+    service = new LivroService();
+    expect(service).toBeTruthy();
+  })
+
+  //AAA arrange act assert
+})
