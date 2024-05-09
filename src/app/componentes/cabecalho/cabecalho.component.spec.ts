1f07eb9f840746cd2b7fafa43aaa312f4c03753d
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-09 16:55:29 -0300

    Vídeo 3.2 - acessando a instância do componente

--- src/app/componentes/cabecalho/cabecalho.component.spec.ts
+++ src/app/componentes/cabecalho/cabecalho.component.spec.ts
@@ -1,18 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CabecalhoComponent } from "./cabecalho.component"
 
 describe('CabecalhoComponent', () => {
   let component: CabecalhoComponent;
+  let fixture: ComponentFixture<CabecalhoComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [CabecalhoComponent],
     })
-    component = new CabecalhoComponent
+    fixture = TestBed.createComponent(CabecalhoComponent);
+    component = fixture.componentInstance;
   });
 
   it('deveria ser criado', () => {
     expect(component).toBeTruthy();
-  })
+  });
+
+  it('deveria definir as propriedades alt e src', () => {
+    expect(component.alt).toBeDefined();
+    expect(component.src).toBeDefined();
+  });
 
 })
