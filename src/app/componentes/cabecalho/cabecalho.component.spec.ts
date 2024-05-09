2a170a31a9e6318ac2487c3cb15c292971593560
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-09 17:32:08 -0300

    Vídeo 3.3 - testes de snapshot

--- src/app/componentes/cabecalho/cabecalho.component.spec.ts
+++ src/app/componentes/cabecalho/cabecalho.component.spec.ts
@@ -22,4 +22,9 @@ describe('CabecalhoComponent', () => {
     expect(component.src).toBeDefined();
   });
 
+  it('deveria renderizar o conteúdo baseado nas propriedades src e alt', () => {
+    component.src = 'https://example.com/test-image.jpg';
+    component.alt = 'Imagem teste';
+    expect(component).toMatchSnapshot();
+  })
 })
