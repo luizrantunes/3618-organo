b03a73dec61419191384818f167ae17ef8338970
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-13 09:38:48 -0300

    Vídeo 5.2 - Testando a adição de um novo livro

--- src/app/paginas/formulario/formulario.component.spec.ts
+++ src/app/paginas/formulario/formulario.component.spec.ts
@@ -34,4 +34,39 @@ describe('FormularioComponent', () => {
       classificacao: null
     });
   })
+
+  it('deveria adicionar um novo livro', () => {
+    const novoLivro = {
+      titulo: 'Novo Livro',
+      autoria: 'Autoria Desconhecida',
+      imagem: 'http://example.com/cover.jpg',
+      genero: 'romance',
+      dataLeitura: '2024-04-19',
+      classificacao: 5
+    };
+
+    const adicionarLivroSpy = jest.spyOn(service, 'adicionarLivro');
+    const routerSpy = jest.spyOn(component['router'], 'navigate');
+
+    component.formulario.setValue(novoLivro);
+    component.adicionarLivro();
+
+    expect(adicionarLivroSpy).toHaveBeenCalledWith({
+      ...novoLivro,
+      genero: component.generos.find(g => g.id === novoLivro.genero),
+    })
+
+    expect(component.formulario.value).toEqual({
+      titulo: null,
+      autoria: null,
+      imagem: null,
+      genero: null,
+      dataLeitura: null,
+      classificacao: null
+    })
+
+    expect(routerSpy).toHaveBeenCalledWith(['lista-livros']);
+  })
+
+
 })
